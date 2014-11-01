47ede0578549785a9011c136b8617f3d1b44af69
Ben Fairless <ben.fairless@landregistry.gsi.gov.uk>
2014-11-01 21:01:50 +0000

    Renamed output field

--- js/main.js
+++ js/main.js
@@ -3,32 +3,33 @@ $( document ).ready( function() {
 
   // CLEAR INPUT FIELDS
   function clearFields() {
-    $( 'input#acct' ).val( "" );
-    $( 'input#pass' ).val( "" );
-    $( 'input#hash' ).val( "" );
-    $( '#hashgroup').fadeOut( 750 );
+    $( 'input#account' ).val( "" );
+    $( 'input#password' ).val( "" );
+    $( 'input#output' ).val( "" );
+    $( 'div#output-group').fadeOut( 750 );
   }
 
   function highlightError() {
     return false;
   }
 
-  // GENERATE HASH FROM INPUT FIELDS
+  // GENERATE OUTPUT FROM INPUT FIELDS AND DISPLAY
   function genHash() {
-    var acct = $( 'input#acct').val();
-    var pass = $( 'input#pass').val();
+    var acct = $( 'input#account').val();
+    var pass = $( 'input#password').val();
     if ( acct === "" || pass === "") {
       highlightError();
     }
     else {
-      $( '#hashgroup' ).fadeIn( 750 );
-      $( '#hash' ).val( shallNotPass.create( acct, pass ) );
-      $( '#hash' ).focus();
-      $( '#hashgroup' ).delay(30000).fadeOut( 750, function() { clearFields(); } );
+      console.log( acct + pass );
+      $( 'div#output-group' ).fadeIn( 750 );
+      $( 'input#output' ).val( shallNotPass.create( acct, pass ) );
+      $( 'input#output' ).focus();
+      $( 'div#output-group' ).delay(30000).fadeOut( 750, function() { clearFields(); } );
     }
   }
 
-  // Generate hash when 'Enter' is pressed in an input field
+  // Generate output when 'Enter' is pressed in an input field
   $( 'form#snp' ).each( function() {
     $( this ).find( 'input' ).keypress( function(e) {
       if ( e.which == 10 || e.which == 13 ) {
@@ -40,11 +41,11 @@ $( document ).ready( function() {
   // Hide JS warning on load
   $( '.jscheck').hide();
   // Hide HASHGROUP on load
-  $( '#hashgroup' ).hide();
+  $( 'div#output-group' ).hide();
   // Auto-focus ACCT
-  $( 'input#acct' ).focus();
+  $( 'input#account' ).focus();
   // Auto-select HASH
-  $( 'input#hash').focus( function() { $( this ).select(); });
+  $( 'input#output').focus( function() { $( this ).select(); });
   // If it looks like a button, and it's called a button...
   $( '.btn' ).button();
   // If it looks like a tooltip, and it's called a tooltip...
